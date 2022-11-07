b31332e1b7501dbbbdfaef5b872bea4d76f7f1f5
AlanRacciatti <alanracciatti1220@gmail.com>
2022-11-06 23:29:27 -0300

    feat: add embeds

--- src/commands/chat/faucet-command.ts
+++ src/commands/chat/faucet-command.ts
@@ -1,5 +1,4 @@
 import { ChatInputCommandInteraction, EmbedBuilder, PermissionsString } from 'discord.js';
-
 import { FaucetNetworkOption } from '../../enums/index.js';
 import { Language } from '../../models/enum-helpers/index.js';
 import { EventData } from '../../models/internal-models.js';
@@ -13,48 +12,54 @@ export class FaucetCommand implements Command {
     public deferType = CommandDeferType.PUBLIC;
     public requireClientPerms: PermissionsString[] = [];
     public async execute(intr: ChatInputCommandInteraction, data: EventData): Promise<void> {
-        let args = {
-            network: intr.options.getString(
-                Lang.getRef('arguments.network', Language.Default)
-            ),
-            token: intr.options.getString(
-                Lang.getRef('arguments.token', Language.Default)
-            ),
-        };
 
-        let { id } = intr.user;
-        let address: string | null = await FaucetUtils.getAddressFromId(id);
-        let canRequestTokens: boolean = await FaucetUtils.getAddressRequestAvailability(address, args.network, args.token);
+        let network = intr.options.getString(
+            Lang.getRef('arguments.network', Language.Default)
+        )
+
+        let token = intr.options.getString(
+            Lang.getRef('arguments.token', Language.Default)
+        )
 
         let embed: EmbedBuilder;
 
-        if (canRequestTokens) {
-            // TODO: Send tokens and show embed faucetTransaction
-            let tx = await FaucetUtils.sendTokens(address, args.network, args.token);
+        let { id } = intr.user;
+        let address: string | null = await FaucetUtils.getAddressFromId(id);
+        let canRequestTokens: boolean = await FaucetUtils.getAddressRequestAvailability(address, network, token);
+        let isTokenSupported: boolean = await FaucetUtils.isTokenSupported(network, token);
+
+        if (!isTokenSupported) {
+            embed = Lang.getEmbed('faucetEmbeds.notSupported', data.lang, {
+                network,
+                token
+            })
         } else {
-            // TODO: Send embed with error
+            if (canRequestTokens) {
+                let faucetHasBalance: boolean = await FaucetUtils.hasBalance(network, token)
+                if (faucetHasBalance) {
+                    let txLink = await FaucetUtils.sendTokens(address, network, token);
+                    embed = Lang.getEmbed('faucetEmbeds.transaction', data.lang, {
+                        network,
+                        token,
+                        amount: FaucetUtils.getAmount(network, token),
+                        link: txLink,
+                        address
+                    });
+                } else {
+                    embed = Lang.getEmbed('faucetEmbeds.noBalance', data.lang, {
+                        network,
+                        token,
+                    });
+                }
+            } else {
+                embed = Lang.getEmbed('faucetEmbeds.notAllowed', data.lang, {
+                    network,
+                    token,
+                    nextRequest: await FaucetUtils.nextRequest(address, network, token)
+                })
+            }
         }
-        switch (args.network) {
-            case FaucetNetworkOption.GOERLI: {
-                embed = Lang.getEmbed('displayEmbeds.faucetTransaction', data.lang, {
-                    network: args.network,
-                    token: args.token,
-                    amount: "1"
-                });
-                break;
-            }
-            case FaucetNetworkOption.MUMBAI: {
-                embed = Lang.getEmbed('displayEmbeds.permissions', data.lang);
-                break;
-            }
-            case FaucetNetworkOption.ALFAJORES: {
-                embed = Lang.getEmbed('displayEmbeds.faq', data.lang);
-                break;
-            }
-            default: {
-                return;
-            }
-        }
+
 
         await InteractionUtils.send(intr, embed);
     }
--- src/utils/faucet-utils.ts
+++ src/utils/faucet-utils.ts
@@ -11,7 +11,7 @@ export class FaucetUtils {
         address: string,
         network: string,
         token: string
-    ) {
+    ): Promise<boolean> {
         return true;
     }
 
@@ -19,7 +19,36 @@ export class FaucetUtils {
         address: string,
         network: string,
         token: string
-    ) {
-        return "0x6864dC5998c25Db320D3370A53592E44a246FFf4";
+    ): Promise<string> {
+        return "https://goerli.etherscan.io/tx/0x24693bb515cd2c987e3eb5dd9c2ed1f95f69ae57aaa0bf1e10fb8a67d755eba6";
+    }
+
+    public static async hasBalance(
+        network: string,
+        token: string
+    ): Promise<boolean> {
+        return true;
+    }
+
+    public static getAmount(
+        network: string,
+        token: string
+    ): string {
+        return "1";
+    }
+
+    public static async nextRequest(
+        address: string,
+        network: string,
+        token: string
+    ): Promise<string> {
+        return "2022-06-07 22:00";
+    }
+
+    public static isTokenSupported(
+        network: string,
+        token: string
+    ): boolean {
+        return true;
     }
 }
